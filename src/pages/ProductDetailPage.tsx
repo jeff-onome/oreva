d890f0649d3c337ed341265b845d3d71d5e6b1ea
jeff <onomejephthah1@gmail.com>
2025-09-28 05:26:56 +0100

    first commit 9/27/25 2

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -404,12 +404,12 @@ const ProductDetailPage: React.FC = () => {
                   ))}
               </div>
           ) : (
-            <p className="text-text-secondary">No reviews yet. Be the first to share your thoughts!</p>
+            <p className="text-text-secondary">No reviews yettttt. Be the first to share your thoughts!</p>
           )}
 
           {/* Add Review Form */}
           {user && hasPurchased && <ReviewForm productId={product.id} onReviewSubmit={handleReviewSubmitted} />}
-          {user && !hasPurchased && <p className="mt-4 p-4 bg-blue-50 text-blue-700 rounded-lg">Youuuu must purchase this item to leave a review.</p>}
+          {user && !hasPurchased && <p className="mt-4 p-4 bg-blue-50 text-blue-700 rounded-lg">You must purchase this item to leave a review.</p>}
           {!user && <p className="mt-4 p-4 bg-neutral rounded-lg">Please <Link to="/login" className="font-bold text-primary hover:underline">log in</Link> to leave a review.</p>}
 
       </div>
